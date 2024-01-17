f83c7fa9b2e99eb1c1a3b88e3506c7c0f60011ce
maddiehutch1 <madhutch@byu.edu>
2024-01-17 11:14:47 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Mission2_DiceCounter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numOfRolls = 0;
+
+        string introLine1 = "Welcome to the dice throwing simulator!";
+        string introLine2 = "How many dice rolls would you like to simulate?";
+
+        string introLine3 = "DICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.";
+        string diceRollLine = "Total number of rolls = ";
+
+        string outroLine = "Thank you for using the dice throwing simulator. Goodbye!";
+
+        System.Console.WriteLine(introLine1 + "\n\n" + introLine2);
+        numOfRolls = int.Parse(System.Console.ReadLine());
+
+        Rolls d = new Rolls();
+
+        System.Console.WriteLine("\n" + introLine3 + "\n" + diceRollLine + numOfRolls);
+
+        // place method here that is connected to the dice and rolls
+        int[,] retrievedArray = d.RollTracker(numOfRolls);
+
+        for (int i = 0; i < retrievedArray.GetLength(0); i++) 
+        {
+            string countMeter = "";
+
+            int percentage = (int)Math.Round(((float)retrievedArray[i, 1] / numOfRolls) * 100);
+
+            for (int j = 0; j < percentage; j++) 
+            {
+                countMeter += "*";
+            }
+
+            Console.WriteLine(retrievedArray[i, 0] + ": " + countMeter);
+        }
+
+        System.Console.WriteLine("\n" + outroLine);
+    }
+}
---
+++ Rolls.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2_DiceCounter
+{
+    internal class Rolls
+    {
+        
+        public int[,] RollTracker(int rollCount)
+        {
+            int[,] trackerRolls = { {2, 0}, {3, 0}, {4, 0},
+                                    {5, 0}, {6, 0}, {7, 0},
+                                    {8, 0}, {9, 0}, {10, 0},
+                                    {11, 0}, {12, 0}};
+                
+            for (int roll = 0; roll < rollCount; roll++)
+            {
+                Random dice = new Random();
+
+                int d1 = dice.Next(1, 7);
+                int d2 = dice.Next(1, 7);
+
+                int totalValue = d1 + d2;
+
+                for (int item = 0; item < trackerRolls.GetLength(0); item++)
+                {
+                    if (trackerRolls[item, 0] == totalValue)
+                    {
+                        trackerRolls[item, 1] += 1;
+                    }
+                }
+            }
+
+            return trackerRolls;
+
+        }
+       
+    }
+}
