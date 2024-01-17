602846edd2b233ea5531dfff2659bd37d1189b6e
maddiehutch1 <madhutch@byu.edu>
2024-01-17 11:28:46 -0700

    added comments to code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿using Mission2_DiceCounter;
+﻿// Madison Hutchings
+// Mission 2 - Dice Counter that shows percentage of times a total num rolled is resulted from two dice
+// being rolled
+
+// the main project is called in order to access all the classes 
+using Mission2_DiceCounter;
 
 internal class Program
 {
+    // MAIN method that holds all the key stuff that is retrieved from and displayed to the client
     private static void Main(string[] args)
     {
+        // initialize a bunch of variables to be organized and/or get ready to be used in loops or called
         int numOfRolls = 0;
 
         string introLine1 = "Welcome to the dice throwing simulator!";
@@ -15,26 +22,35 @@ internal class Program
         string outroLine = "Thank you for using the dice throwing simulator. Goodbye!";
 
         System.Console.WriteLine(introLine1 + "\n\n" + introLine2);
+        
+        // value retrieved from user's input
         numOfRolls = int.Parse(System.Console.ReadLine());
 
+        // create new roll instance when user's input is retrieved
         Rolls d = new Rolls();
 
+        // dynamically displays amount of rolls inputed by user
         System.Console.WriteLine("\n" + introLine3 + "\n" + diceRollLine + numOfRolls);
 
-        // place method here that is connected to the dice and rolls
+        // place method here that is connected to the dice and rolls... calls the RollTracker method
+        // retrieves an array of arrays
         int[,] retrievedArray = d.RollTracker(numOfRolls);
 
+        // use for loop to run through the array taken from what the method returned
         for (int i = 0; i < retrievedArray.GetLength(0); i++) 
         {
             string countMeter = "";
 
+            // do math... but initializes the result to an integer in order to be used in loop below
             int percentage = (int)Math.Round(((float)retrievedArray[i, 1] / numOfRolls) * 100);
 
+            // using the percentage to know how many astrisks should be added
             for (int j = 0; j < percentage; j++) 
             {
                 countMeter += "*";
             }
 
+            // display the histogram!
             Console.WriteLine(retrievedArray[i, 0] + ": " + countMeter);
         }
 
--- Rolls.cs
+++ Rolls.cs
@@ -8,14 +8,16 @@ namespace Mission2_DiceCounter
 {
     internal class Rolls
     {
-        
+        // method returns an array of arrays and uses rollCount input as a parameter
         public int[,] RollTracker(int rollCount)
         {
+            // initialize array of arrays
             int[,] trackerRolls = { {2, 0}, {3, 0}, {4, 0},
                                     {5, 0}, {6, 0}, {7, 0},
                                     {8, 0}, {9, 0}, {10, 0},
                                     {11, 0}, {12, 0}};
                 
+            // use for loop to simulate two dice being rolled, added up to find total
             for (int roll = 0; roll < rollCount; roll++)
             {
                 Random dice = new Random();
@@ -25,6 +27,7 @@ namespace Mission2_DiceCounter
 
                 int totalValue = d1 + d2;
 
+                // for loop is a counter for the num of times the total shows up
                 for (int item = 0; item < trackerRolls.GetLength(0); item++)
                 {
                     if (trackerRolls[item, 0] == totalValue)
@@ -34,6 +37,7 @@ namespace Mission2_DiceCounter
                 }
             }
 
+            // return the updated array of arrays
             return trackerRolls;
 
         }
